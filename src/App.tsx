ceb28814389f5cc28745a03a6559d521d4a23d91
msa <serg.ks@gmail.com>
2023-03-07 17:42:07 +0400

    start: calc layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import css from './App.module.css'
+import {SuperBtn} from "./components/SuperBtn/SuperBtn";
+import eye from "./assets/img/eye.svg"
+import selector from "./assets/img/selector.svg"
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [toggle, setToggle] = useState(true)
+
+    return (
+        <div className={css.app}>
+            <div className={css.blocks}>
+                <div className={css.block}>
+                    <div className={css.tablo}>
+                        <div className={css.display}>
+                            1000 000
+                        </div>
+                    </div>
+                    <div className={css.tablo}>
+                        <SuperBtn>/</SuperBtn>
+                        <SuperBtn>x</SuperBtn>
+                        <SuperBtn>-</SuperBtn>
+                        <SuperBtn>+</SuperBtn>
+                    </div>
+                    <div className={css.gridTablo}>
+                        <SuperBtn>7</SuperBtn>
+                        <SuperBtn>8</SuperBtn>
+                        <SuperBtn>9</SuperBtn>
+                        <SuperBtn>4</SuperBtn>
+                        <SuperBtn>5</SuperBtn>
+                        <SuperBtn>6</SuperBtn>
+                        <SuperBtn>1</SuperBtn>
+                        <SuperBtn>2</SuperBtn>
+                        <SuperBtn>3</SuperBtn>
+                        <SuperBtn style={{gridRow: '4', gridColumnStart: "1", gridColumnEnd: "3"}}>0</SuperBtn>
+                        <SuperBtn>,</SuperBtn>
+                    </div>
+                    <div className={css.tablo}>
+                        <SuperBtn xType={"secondary"}>=</SuperBtn>
+                    </div>
+                </div>
+                <div>
+                    <div className={css.toggler}>
+                        <button onClick={()=>setToggle(!toggle)} disabled={toggle} className={css.toggleButton}><img src={eye} alt="eye"/> Runtime</button>
+                        <button onClick={()=>setToggle(!toggle)} disabled={!toggle} className={css.toggleButton}><img src={selector} alt="selector"/>Constructor</button>
+                    </div>
+                    <div className={css.block}>
+                        bbb
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/SuperBtn/SuperBtn.tsx
@@ -0,0 +1,28 @@
+import React, {ButtonHTMLAttributes, DetailedHTMLProps} from 'react'
+import css from './SuperBtn.module.css'
+
+type DefaultButtonPropsType = DetailedHTMLProps<ButtonHTMLAttributes<HTMLButtonElement>,
+    HTMLButtonElement>
+
+type SuperButtonPropsType = DefaultButtonPropsType & {
+    xType?: string
+}
+
+export const SuperBtn: React.FC<SuperButtonPropsType> = (
+    {
+        xType,
+        className,
+        disabled,
+        ...restProps // все остальные пропсы попадут в объект restProps, там же будет children
+    }
+) => {
+    const finalClassName = `${css.button} ${xType === 'red' ? css.red : xType === 'secondary' ? css.secondary : css.default} ${disabled ? css.disabled : ''} ${className ? className : ''}`;
+
+    return (
+        <button
+            disabled={disabled}
+            className={finalClassName}
+            {...restProps} // отдаём кнопке остальные пропсы если они есть (children там внутри)
+        />
+    )
+}
